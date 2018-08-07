1c1ec50f6b38306865f86f1b7bc013b22c8792f9
dillhicks <33649660+dillhicks@users.noreply.github.com>
2018-08-07 16:55:05 -0700

    fix tab syntax errors

--- Tools/annotate.py
+++ Tools/annotate.py
@@ -8,22 +8,20 @@ import argparse
 import os
 
 def load_labels(label_file):
-  label = []
-  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
-  for l in proto_as_ascii_lines:
-    label.append(l.rstrip())
-return label
+	label = []
+	proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+	for l in proto_as_ascii_lines:
+		label.append(l.rstrip())
+	return label
 
 def load_graph(model_file):
 	graph = tf.Graph()
 	graph_def = tf.GraphDef()
-
 	with open(model_file, "rb") as f:
 		graph_def.ParseFromString(f.read())
 	with graph.as_default():
 		tf.import_graph_def(graph_def)
-
-return graph
+	return graph
 
 
 def read_tensor_from_image_file(file_name,
@@ -35,27 +33,23 @@ def read_tensor_from_image_file(file_name,
 	output_name = "normalized"
 	file_reader = tf.read_file(file_name, input_name)
 	if file_name.endswith(".png"):
-	image_reader = tf.image.decode_png(
-		file_reader, channels=3, name="png_reader")
+		image_reader = tf.image.decode_png(file_reader, channels=3, name="png_reader")
 	elif file_name.endswith(".gif"):
-	image_reader = tf.squeeze(
-		tf.image.decode_gif(file_reader, name="gif_reader"))
+		image_reader = tf.squeeze(tf.image.decode_gif(file_reader, name="gif_reader"))
 	elif file_name.endswith(".bmp"):
-	image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
+		image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
 	else:
-	image_reader = tf.image.decode_jpeg(
-	    file_reader, channels=3, name="jpeg_reader")
+		image_reader = tf.image.decode_jpeg(file_reader, channels=3, name="jpeg_reader")
 	float_caster = tf.cast(image_reader, tf.float32)
 	dims_expander = tf.expand_dims(float_caster, 0)
 	resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
 	normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
 	sess = tf.Session()
 	result = sess.run(normalized)
+	return result
 
-return result
 
-
-if __name__ == "__main__"
+if __name__ == "__main__":
 
 	file_name = "tensorflow/examples/label_image/data/grace_hopper.jpg"
 	model_file = \
@@ -67,9 +61,6 @@ if __name__ == "__main__"
 	input_std = 255
 	input_layer = "input"
 	output_layer = "InceptionV3/Predictions/Reshape_1"
-
-
-
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--images", help="image directory to be processed")
