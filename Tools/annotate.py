e555bbe7490f58970e738957f1fc94cd33e565ec
dillhicks <33649660+dillhicks@users.noreply.github.com>
2018-08-07 16:07:24 -0700

    load label function

--- Tools/annotate.py
+++ Tools/annotate.py
@@ -7,6 +7,13 @@ import tensorflow as tf
 import argparse
 import os
 
+def load_labels(label_file):
+  label = []
+  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+  for l in proto_as_ascii_lines:
+    label.append(l.rstrip())
+return label
+
 def load_graph(model_file):
 	graph = tf.Graph()
 	graph_def = tf.GraphDef()
