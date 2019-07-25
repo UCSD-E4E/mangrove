d1ec53f551ae234132ab229c4c541db8464cd731
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2019-07-25 17:50:16 -0400

    tune

--- classify.py
+++ classify.py
@@ -11,10 +11,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import cross_val_score, KFold, GridSearchCV
 import argparse
 import glob
 import cv2
+
+def grid_search_params(grid, svc, data, target):
+    clf = GridSearchCV(svc, grid, cv=5)
+    print('[STATUS] Beginning grid search...')
+    clf.fit(data, target)
+    print(clf.best_params_)
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
@@ -25,6 +31,7 @@ if __name__=='__main__':
     parser.add_argument('-r', '--retrain', action='store_true', help='retrain the model and save it')
     parser.add_argument('--show', action='store_true', help='show the images with their predicted labels')
     parser.add_argument('--analyze', action='store_true')
+    parser.add_argument('--gs', action='store_true', help='grid search SVM params')
     args = parser.parse_args()
 
     train_path = '/home/sam/Documents/e4e/mvnm_feature_based/dataset/train'
@@ -51,7 +58,7 @@ if __name__=='__main__':
         elif args.model=='knn':
             clf = KNeighborsClassifier()
         elif args.model=='svm':
-            clf = SVC(gamma='auto')
+            clf = SVC(gamma=0.001, C=10)
         clf.fit(features, labels)
     else:
         if args.model=='rf':
@@ -70,6 +77,8 @@ if __name__=='__main__':
         in_dirs = os.listdir(in_path)
         in_dirs.sort()
         for d in in_dirs:
+            if d == 'm':
+                continue
             images = glob.glob(os.path.join(in_path, d, '*.jpg',))
             im_count = 0
             correct = 0
@@ -80,12 +89,12 @@ if __name__=='__main__':
                 feature = extractor.extract(img)
                 feature = sc.transform(feature.reshape(1, -1))
                 prediction = clf.predict(feature)
-                print(prediction)
                 prediction_label = le.inverse_transform(prediction)[0]
                 im_count += 1
                 if d == prediction_label:
                     correct += 1
-
+                if im_count % 10 ==0:
+                    print(im_count)
                 if args.show:
                     cv2.putText(img_bgr, prediction_label, (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,255), 3)
                     cv2.imshow('image', img_bgr)
@@ -94,14 +103,17 @@ if __name__=='__main__':
                         break
             print('Actual {}: {}/{} ({}%) labeled as {}'.format(d, correct, im_count, correct/im_count*100, d))
     elif args.analyze:
-            labels = le.inverse_transform(labels)
-            reduced = TSNE(n_components=2, random_state=6).fit_transform(features)
-            m_reduced = reduced[labels=='m'].T
-            nm_reduced = reduced[labels=='nm'].T
-            plt.scatter(m_reduced[0], m_reduced[1])
-            plt.scatter(nm_reduced[0], nm_reduced[1])
-            plt.show()
-            
+        labels = le.inverse_transform(labels)
+        reduced = TSNE(n_components=2, random_state=6).fit_transform(features)
+        m_reduced = reduced[labels=='m'].T
+        nm_reduced = reduced[labels=='nm'].T
+        plt.scatter(m_reduced[0], m_reduced[1])
+        plt.scatter(nm_reduced[0], nm_reduced[1])
+        plt.show()
+    elif args.gs and args.model=='svm':
+        grid = {'kernel':['rbf', 'poly']}
+        grid_search_params(grid, svc=clf, data=features, target=labels)
+                
     if args.retrain:
         if args.model=='rf':
             joblib.dump(clf, os.path.join(out_path, 'rf.joblib'))
---
+++ filter_water.py
@@ -0,0 +1,7 @@
+import numpy as np
+from extract import CNNFeatureExtractor
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+import h5py
+
