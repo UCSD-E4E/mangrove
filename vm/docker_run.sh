6c1b01d702c21b9695031d846324996ff770acc2
Arden <awma@ucsd.edu>
2019-07-26 15:25:16 -0700

    bugfixes for vm scripts

--- vm/docker_run.sh
+++ vm/docker_run.sh
@@ -1,9 +1,9 @@
 docker pull awmaucsd/retrain
 
 echo "Note: GPUs are typicall indexed starting at 1, i.e. 2 GPUS will be 0,1"
-read -p "Which GPU: " gpu_num
+read -N 1 -s -p "Which GPU: " gpu_num
 
-docker run -it --gpus '"device=$gpu_num"' \
+docker run -it --gpus "device=$gpu_num" \
     -v $(pwd)/input:/input \
     -v $(pwd)/output:/output \
     awmaucsd/retrain
