ac4557ce1752d1e7b9ed3d070ab1014d2715f2af
Arden <awma@ucsd.edu>
2019-07-29 14:29:11 -0700

    Fixed typos and compatability issues

--- CNN Development/retrain_docker.sh
+++ CNN Development/retrain_docker.sh
@@ -9,7 +9,7 @@ read -p 'Batch Size: ' batch_size
 python3 cnn_retrain2.py \
     --image_dir=/mnt/input \
     --output_graph=/mnt/output/output_graph_v$version.pb \
-    --output_lables=/mnt/output/output_labels_v$version.pb \
+    --output_labels=/mnt/output/output_labels_v$version.pb \
     --how_many_training_steps=$epochs \
     --learning_rate=$learning_rate \
     --train_batch_size=$batch_size \
--- vm/run_docker.sh
+++ vm/run_docker.sh
@@ -1,7 +1,7 @@
 docker pull awmaucsd/retrain
 
 echo "Note: GPUs are typicall indexed starting at 1, i.e. 2 GPUS will be 0,1"
-read -N 1 -s -p "Which GPU: " gpu_num
+read -p "Which GPU: " gpu_num
 
 docker run -it --name cnn_app -u=0 --gpus "device=$gpu_num" \
     -v $(pwd)/input:/mnt/input \
