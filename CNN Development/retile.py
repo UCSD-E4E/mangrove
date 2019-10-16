c8012da4b63ab8470f888dede7040253c93dd553
dillhicks <sdhicks@ucsd.edu>
2019-10-16 11:55:29 -0700

    add shp masking to retile

--- CNN Development/retile.py
+++ CNN Development/retile.py
@@ -1,8 +1,8 @@
 import argparse
 import os
 import subprocess
-from PIL import Image
 from tqdm import tqdm
+from PIL import Image
 parser = argparse.ArgumentParser(description="Retile an orthomosiac, call this in the same folder as the orthomosaic")
 parser.add_argument("--width",help = "Width of output tiles")
 parser.add_argument("--input", help = "input orthomosaic")
@@ -15,17 +15,29 @@ if args.input:
 	img_input = args.input
 if args.targetDir:
 	outputDir = args.targetDir
+if args.shpfile:
+	polygon = args.targetDir
+else:
+	polygon == False
 
 cwd = os.getcwd()
 if os.path.exists(os.path.join(cwd,outputDir)) == False:
 	os.mkdir(os.path.join(cwd,outputDir))
+
+if isinstance(polygon, str):
+	polycall = "gdal -clip " + polygon + " " + img_input + " " + "clipped" + img_input
+	img_input = "clipped" + img_input
+	subprocess.call(polycall, shell=True)
+
+
 
 call = "gdal_retile.py -ps " + out_width + " " + out_width + " " + "-targetDir " + outputDir + " " + img_input
 print(call)
 subprocess.call(call, shell=True)
 
 img_dir = os.path.join(cwd, outputDir)
-print("Filtering through images")
+print("Removing undersized tiles in:")
+print(img_dir)
 for filename in tqdm(os.listdir(img_dir)):
 	filepath = os.path.join(img_dir, filename)
 	if os.path.splitext(filename)[1] == ".tif":
