18b7534151a9fc499666b3bfaa1d2ab616598632
Arden <awma@ucsd.edu>
2020-05-06 23:04:00 -0700

    bug fix

--- Segmentation-Tools/split_vector.py
+++ Segmentation-Tools/split_vector.py
@@ -11,17 +11,15 @@ def split_vector(filepath):
     # Creating necessary directories
 	m_dir = os.path.join(dir_path, "m")
 	nm_dir = os.path.join(dir_path, "nm")
-	if not os.path.exists(m_dir):
-		os.mkdir(m_dir)
-	if not os.path.exists(nm_dir):
-		os.mkdir(nm_dir)
+	if os.path.exists(m_dir):
+		os.system("rm -r " + m_dir)
+	if os.path.exists(nm_dir):
+		os.system("rm -r " + nm_dir)
+	os.mkdir(m_dir)
+	os.mkdir(nm_dir)
 
 	with fiona.open(filepath) as source:
-		# Making sure m.shp does not already exist
 		m_file = os.path.join(m_dir, "m.shp")
-		if (os.path.exists(m_dir)):
-			#os.remove("m.shp")	
-			os.system("rm -r" + m_dir)
 		# Joining mangrove polygons
 		with fiona.open(m_file, "w", crs=source.crs, driver=source.driver, schema=source.schema) as m_shp:
 			print("Creating mangrove files in " + m_dir)
@@ -30,11 +28,7 @@ def split_vector(filepath):
 					m_shp.write(record)
 			print("Joined {} mangrove polygons.".format(len(m_shp)))
 
-		# Making sure m.shp does not already exist
 		nm_file = os.path.join(nm_dir, "nm.shp")
-		if (os.path.exists(nm_dir)):
-			#os.remove("nm.shp")	
-			os.system("rm -r" + nm_dir)
 		# Joining nonmangrove polygons
 		with fiona.open(nm_file, "w", crs=source.crs, driver=source.driver, schema=source.schema) as nm_shp:
 			print("Creating nonmangrove files in " + nm_dir)
