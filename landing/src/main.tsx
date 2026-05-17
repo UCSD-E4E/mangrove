a9d3aa0ef2c47b8a72a8605cdd8be06a40078343
choshingcheung <adyt0724@gmail.com>
2026-05-16 19:31:20 -0700

    Fix React Router basename for GitHub Pages subpath deployment

--- landing/src/main.tsx
+++ landing/src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>,
